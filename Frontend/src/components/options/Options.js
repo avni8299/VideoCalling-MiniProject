bc9300702ba1f673eb8d023c9d2777f119262b8a
m-triple-m <triplem656@mail.com>
2022-12-08 00:28:15 +0530

    ..

--- Frontend/src/components/options/Options.js
+++ Frontend/src/components/options/Options.js
@@ -1,32 +1,22 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
-import { Input, Button, Tooltip, Modal, message } from "antd";
-import Phone from "../../assests/phone.gif";
-import Teams from "../../assests/teams.mp3";
-import classes from "./Options.module.css";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import VideoContext from "../../context/VideoContext";
-import Hang from "../../assests/hang.svg";
-import {
-  TwitterIcon,
-  TwitterShareButton,
-  WhatsappShareButton,
-  WhatsappIcon,
-  FacebookIcon,
-  FacebookShareButton,
-} from "react-share";
-import {
-  UserOutlined,
-  CopyOutlined,
-  InfoCircleOutlined,
-  PhoneOutlined,
-} from "@ant-design/icons";
-import { socket } from "../../context/VideoState";
+import React, { useState, useContext, useEffect, useRef } from "react"
+import { Input, Button, Tooltip, Modal, message } from "antd"
+import Phone from "../../assests/phone.gif"
+import Teams from "../../assests/teams.mp3"
+// import classes from "./Options.module.css";
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import VideoContext from "../../context/VideoContext"
+import Hang from "../../assests/hang.svg"
+import Swal from "sweetalert2"
+
+import { TwitterIcon, TwitterShareButton, WhatsappShareButton, WhatsappIcon, FacebookIcon, FacebookShareButton } from "react-share"
+import { UserOutlined, CopyOutlined, InfoCircleOutlined, PhoneOutlined } from "@ant-design/icons"
+import { socket } from "../../context/VideoState"
 
 const Options = () => {
-  const [idToCall, setIdToCall] = useState("");
+  const [idToCall, setIdToCall] = useState("")
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const Audio = useRef();
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const Audio = useRef()
   const {
     call,
     callAccepted,
@@ -43,176 +33,137 @@ const Options = () => {
     otherUser,
     setOtherUser,
     leaveCall1,
-  } = useContext(VideoContext);
+  } = useContext(VideoContext)
 
   useEffect(() => {
     if (isModalVisible) {
-      Audio?.current?.play();
-    } else Audio?.current?.pause();
-  }, [isModalVisible]);
+      Audio?.current?.play()
+    } else Audio?.current?.pause()
+  }, [isModalVisible])
 
   const showModal = (showVal) => {
-    setIsModalVisible(showVal);
-  };
+    setIsModalVisible(showVal)
+  }
 
   const handleCancel = () => {
-    setIsModalVisible(false);
-    leaveCall1();
-    window.location.reload();
-  };
+    setIsModalVisible(false)
+    leaveCall1()
+    window.location.reload()
+  }
   useEffect(() => {
     if (call.isReceivingCall && !callAccepted) {
-      setIsModalVisible(true);
-      setOtherUser(call.from);
-    } else setIsModalVisible(false);
-  }, [call.isReceivingCall]);
+      setIsModalVisible(true)
+      setOtherUser(call.from)
+    } else setIsModalVisible(false)
+  }, [call.isReceivingCall])
 
   return (
-    <div className={classes.options}>
-      <div style={{ marginBottom: "0.5rem" }}>
-        <h2>Account Info</h2>
-        <Input
-          size="large"
-          placeholder="Your name"
-          prefix={<UserOutlined />}
-          maxLength={15}
-          suffix={<small>{name.length}/15</small>}
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-            localStorage.setItem("name", e.target.value);
-          }}
-          className={classes.inputgroup}
-        />
+    <div className="options-body p-4">
+      <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <p className="text-white h5">Account Info</p>
+          <div className="option">
+            <div className="input-group">
+              <input
+                placeholder="Your name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value)
+                  localStorage.setItem("name", e.target.value)
+                }}
+                className="form-control"
+              />
+              <CopyToClipboard text={me}>
+                  <button
+                    className="btn btn-primary"
+                    tabIndex="0"
+                    onClick={() => {
+                      Swal.fire({
+                        icon: "success",
+                        text: "Code copied successfully",
+                      })
+                    }}>
+                    <i class="fas fa-copy"></i>
+                  </button>
+                </CopyToClipboard>
 
-        <div className={classes.share_options}>
-          <CopyToClipboard text={me}>
-            <Button
-              type="primary"
-              icon={<CopyOutlined />}
-              className={classes.btn}
-              tabIndex="0"
-              onClick={() => message.success("Code copied successfully!")}
-            >
-              Copy code
-            </Button>
-          </CopyToClipboard>
+              <div>
+                
 
-          <div className={classes.share_social}>
-            <WhatsappShareButton
-              url={`https://video-chat-mihir.vercel.app/`}
-              title={`Join this meeting with the given code "${me}"\n`}
-              separator="Link: "
-              className={classes.share_icon}
-            >
-              <WhatsappIcon size={26} round />
-            </WhatsappShareButton>
-            <FacebookShareButton
-              url={`https://video-chat-mihir.vercel.app/`}
-              title={`Join this meeting with the given code "${me}"\n`}
-              className={classes.share_icon}
-            >
-              <FacebookIcon size={26} round />
-            </FacebookShareButton>
-            <TwitterShareButton
-              url={`https://video-chat-mihir.vercel.app/`}
-              title={`Join this meeting with the given code  "${me}"\n`}
-              className={classes.share_icon}
-            >
-              <TwitterIcon size={26} round className={classes.share_border} />
-            </TwitterShareButton>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <p className="text-white h5">Make a call</p>
+          <div className="option">
+            <div className="input-group">
+              <input
+                placeholder="Enter code to call"
+                className="form-control"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+              />
+
+              {callAccepted && !callEnded ? (
+                <button onClick={leaveCall} className="btn btn-danger" tabIndex="0">
+                  <img src={Hang} alt="hang up" style={{ height: "15px" }} />
+                  &nbsp; Hang up
+                </button>
+              ) : (
+                <button
+                  type="primary"
+                  icon={<PhoneOutlined />}
+                  onClick={() => {
+                    if (name.length) callUser(idToCall)
+                    else
+                      Swal.fire({
+                        icon: "success",
+                        text: "Please enter your name to call!",
+                      })
+                  }}
+                  className="btn btn-success"
+                  tabIndex="0">
+                  <i class="fas fa-phone    "></i>
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
-      <div style={{ marginBottom: "0.5rem" }}>
-        <h2>Make a call</h2>
-
-        <Input
-          placeholder="Enter code to call"
-          size="large"
-          className={classes.inputgroup}
-          value={idToCall}
-          onChange={(e) => setIdToCall(e.target.value)}
-          style={{ marginRight: "0.5rem", marginBottom: "0.5rem" }}
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          suffix={
-            <Tooltip title="Enter code of the other user">
-              <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
-            </Tooltip>
-          }
-        />
-
-        {callAccepted && !callEnded ? (
-          <Button
-            variant="contained"
-            onClick={leaveCall}
-            className={classes.hang}
-            tabIndex="0"
-          >
-            <img src={Hang} alt="hang up" style={{ height: "15px" }} />
-            &nbsp; Hang up
-          </Button>
-        ) : (
-          <Button
-            type="primary"
-            icon={<PhoneOutlined />}
-            onClick={() => {
-              if (name.length) callUser(idToCall);
-              else message.error("Please enter your name to call!");
-            }}
-            className={classes.btn}
-            tabIndex="0"
-          >
-            Call
-          </Button>
-        )}
       </div>
 
       {call.isReceivingCall && !callAccepted && (
         <>
           <audio src={Teams} loop ref={Audio} />
-          <Modal
-            title="Incoming Call"
-            visible={isModalVisible}
-            onOk={() => showModal(false)}
-            onCancel={handleCancel}
-            footer={null}
-          >
+          <Modal title="Incoming Call" visible={isModalVisible} onOk={() => showModal(false)} onCancel={handleCancel} footer={null}>
             <div style={{ display: "flex", justifyContent: "space-around" }}>
               <h1>
-                {call.name} is calling you:{" "}
-                <img
-                  src={Phone}
-                  alt="phone ringing"
-                  className={classes.phone}
-                  style={{ display: "inline-block" }}
-                />
+                {call.name} is calling you: <img src={Phone} alt="phone ringing" style={{ display: "inline-block" }} />
               </h1>
             </div>
-            <div className={classes.btnDiv}>
+            <div>
               <Button
                 variant="contained"
-                className={classes.answer}
                 color="#29bb89"
                 icon={<PhoneOutlined />}
                 onClick={() => {
-                  answerCall();
-                  Audio.current.pause();
+                  answerCall()
+                  Audio.current.pause()
                 }}
-                tabIndex="0"
-              >
+                tabIndex="0">
                 Answer
               </Button>
               <Button
                 variant="contained"
-                className={classes.decline}
                 icon={<PhoneOutlined />}
                 onClick={() => {
-                  setIsModalVisible(false);
-                  Audio.current.pause();
+                  setIsModalVisible(false)
+                  Audio.current.pause()
                 }}
-                tabIndex="0"
-              >
+                tabIndex="0">
                 Decline
               </Button>
             </div>
@@ -220,7 +171,7 @@ const Options = () => {
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Options;
+export default Options
